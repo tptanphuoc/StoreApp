ac549ef7a60bd562d3f46fc00f85a0d9ad4e3588
tptanphuoc <huynhphuoc2468@gmail.com>
2023-10-30 02:53:36 +0700

    first commit

---
+++ src/main/java/StoreApp/StoreApp/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package StoreApp.StoreApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import StoreApp.StoreApp.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+	
+	Category getById(int id);
+
+}
